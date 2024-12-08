951eacf67a093d986e93fd850faf65061648e49f
kks2140501 <kks2140501@naver.com>
2024-12-08 19:30:37 +0900

    화면 디자인 및 app router버전의 화면 세팅

---
+++ src/components/searchbar.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import style from "./serachbar.module.css";
+
+export default function Searchbar() {
+  const router = useRouter();
+  //app router 인스턴스 형식에는 쿼리라는 프로퍼티가 없다.
+  // 현재 페이지에 전달된 쿼리 스트링의 값을 꺼내올수 있는 기능 제공
+  const searchParams = useSearchParams();
+  const [search, setSearch] = useState("");
+
+  const q = searchParams.get("q");
+
+  useEffect(() => {
+    setSearch(q || "");
+  }, [q]);
+
+  const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const onSubmit = () => {
+    if (!search || q === search) return; //검색어가 없거나 현재 페이지와 같은 검색어일 경우 리턴
+    router.push(`/search?q=${search}`);
+  };
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      onSubmit();
+    }
+  };
+
+  return (
+    <div className={style.container}>
+      <input value={search} onChange={onChangeSearch} onKeyDown={onKeyDown} />
+      <button onClick={onSubmit}>검색</button>
+    </div>
+  );
+}
