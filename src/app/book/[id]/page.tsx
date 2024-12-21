7a78336f54ff5a892c2539e448c64f9135f73d77
kks2140501 <kks2140501@naver.com>
2024-12-21 21:32:31 +0900

    server action 을 통한 리뷰 등록 기능 구현

--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { notFound } from "next/navigation";
 import style from "./page.module.css";
+import { createReviewAction } from "@/actions/create.review.action";
 
 // export const dynamicParams = false; //  파라미터 이외의 값은 404
 // 정적인 파라미터 생성(동적 경로로 같은 페이지를 static 페이지로 설정)
@@ -7,13 +8,9 @@ export function generateStaticParams() {
   return [{ id: "1" }, { id: "2" }, { id: "3" }];
 }
 
-export default async function Page({
-  params,
-}: {
-  params: Promise<{ id: string | string[] }>;
-}) {
+async function BookDetail({ bookId }: { bookId: string }) {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${(await params).id}`
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${bookId}`
   );
 
   if (!response.ok) {
@@ -28,7 +25,7 @@ export default async function Page({
     book;
 
   return (
-    <div className={style.container}>
+    <section>
       <div
         className={style.cover_img_container}
         style={{ backgroundImage: `url('${coverImgUrl}')` }}
@@ -41,6 +38,33 @@ export default async function Page({
         {author} | {publisher}
       </div>
       <div className={style.description}>{description}</div>
+    </section>
+  );
+}
+
+// hidden만 사용한다면 onChange 함수에 대한 오류 발생으로 readOnly 까지 사용!
+function ReviewEditor({ bookId }: { bookId: string }) {
+  return (
+    <section>
+      <form action={createReviewAction}>
+        <input name="bookId" value={bookId} hidden readOnly />
+        <input required name="content" placeholder="리뷰 내용" />
+        <input required name="author" placeholder="작성자" />
+        <button type="submit">작성하기</button>
+      </form>
+    </section>
+  );
+}
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  return (
+    <div className={style.container}>
+      <BookDetail bookId={(await params).id} />
+      <ReviewEditor bookId={(await params).id} />
     </div>
   );
 }
